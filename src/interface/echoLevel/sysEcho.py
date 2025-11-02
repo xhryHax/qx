8e22b322c37a2311b4dbbbac635660a8e087a8df
xhryHax <138666633+xhryHax@users.noreply.github.com>
2025-11-02 17:16:41 +0800

    Update sysEcho.py

--- src/interface/echoLevel/sysEcho.py
+++ src/interface/echoLevel/sysEcho.py
@@ -21,8 +21,8 @@ class Echo_sys_404(MiddleEcho):
 class Echo_sys_missing(MiddleEcho):
 
     _targetCache = '''<!DOCTYPE html><html lang="cn"><head><meta charset="UTF-8"><title>首页</title>
-                        </head><body><h1 style="text-align: center;">hello word</h1><hr>
-                        <h2 style="text-align: center;">qx</h2></body></html>'''.encode('utf-8')
+                        </head><body>
+                        <h2 style="text-align: center;">qx</h2><hr></body></html>'''.encode('utf-8')
     
     def possible(self, method: str, path: str, target: str, header: dict[str, list[str]]) -> bool:
         if path == '/' and target == '':
